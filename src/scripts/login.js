4428452e30eb1c8b8dd06fca9058d78126f97775
LauenMohidin <lauen.mohidin@yh.nackademin.se>
2025-04-02 12:10:21 +0200

    index.html, login.html, register.html, style.css, login.js

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,23 +1,47 @@
+import { getBaseUrl } from "./api.js";  
 document.addEventListener("DOMContentLoaded", initLogin);
 
 function initLogin() {
   const loginForm = document.getElementById("loginForm");
 
-  loginForm.addEventListener("submit", (event) => {
+  loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    handleLogin();
+    await handleLogin();
   });
 }
 
-function handleLogin() {
-  const username = document.getElementById("username").value;
+async function handleLogin() {
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  // Basic demo login - NOT SECURE
-  if (username === "admin" && password === "admin") {
-    window.location.href = "admin.html";
-  } else {
-    alert("Invalid credentials: LOGGING IN ANYWAY");
-    window.location.href = "admin.html";
+  const loginData = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    const response = await fetch(`${getBaseUrl()}api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(loginData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+
+      
+      localStorage.setItem("user", JSON.stringify(result));
+
+      
+      window.location.href = "index.html";
+    } else {
+      alert("Fel e-post eller lösenord.");
+    }
+  } catch (error) {
+    console.error("Inloggningsfel:", error);
+    alert("Ett tekniskt fel uppstod vid inloggningen.");
   }
 }
+
