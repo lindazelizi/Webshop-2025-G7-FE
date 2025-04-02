e9e21c498d1813a5c9ee853a126704d8b5ea9910
Herman0404 <herminator04@gmail.com>
2025-04-02 07:56:48 +0200

    update

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,7 +1,45 @@
-function displayCartItems() {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+document.addEventListener("DOMContentLoaded", function () {
+    loadCartItems();
+});
+
+function loadCartItems() {
+    const cartContainer = document.getElementById("cart-items");
+    cartContainer.innerHTML = "";
+
+    try {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+        if (cart.length > 0) {
+            cart.forEach((item) => {
+                const cartCard = createCartCard(item);
+                cartContainer.appendChild(cartCard);
+            });
+        }
+    }
+    catch (error) {
+        console.error("Error fetching items:", error);
+        cartContainer.innerHTML = "<p>Failed to load items.</p>";
+    }
 }
 
-function createCartCard() {
+function createCartCard(item) {
+    const element = document.createElement("div");
+    const product = item.product;
+    console.log(product)
+    const quantity = item.quantity;
+    element.className = "item-card";
+    element.innerHTML = `
+    <div id="item-info">
+        <h3>${product.name}</h3>
+        <h3>${product.price} kr</h3>
+    </div>
+  
+  <div id="item-quantity">
+    <button class="increase">+</button>
+    <h3>${quantity} st</h3>
+    <button class="decrease">-</button>
+  </div>
+  `;
 
+    return element;
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,12 @@
 import { fetchProducts, addProducts, checkAdmin } from "../utils/api.js";
 
+// Runs once site is loaded
 document.addEventListener("DOMContentLoaded", function () {
   loadProducts();
-  cartBalanceUpdate();
+  updateCartItems();
 });
+
+// Runs once submit button is pressed
 document.getElementById("addProduct").addEventListener("submit", function (e) {
   e.preventDefault();
   addProduct();
@@ -33,25 +36,9 @@ async function loadProducts() {
   }
 }
 
-async function addProduct() {
-  try {
-    const product = {
-      name: document.getElementById("name").value,
-      price: document.getElementById("price").value,
-      description: document.getElementById("description").value,
-      stock: document.getElementById("stock").value
-    };
-    console.log(product);
-    // HAR INGEN ADMIN TOKEN D:
-    addProducts()
-  } catch (error) {
-    console.error("Error adding product:", error)
-  }
-}
 
 // Function to create an individual product card
 function createProductCard(product) {
-
   const element = document.createElement("div");
   element.className = "product-card";
   element.innerHTML = `
@@ -67,38 +54,95 @@ function createProductCard(product) {
 }
 
 
+// Function to add product into api
+async function addProduct() {
+  try {
+    const product = {
+      name: document.getElementById("name").value,
+      price: document.getElementById("price").value,
+      description: document.getElementById("description").value,
+      stock: document.getElementById("stock").value
+    };
+    console.log(product);
+    // No admin token currently D:
+    addProducts()
+  } catch (error) {
+    console.error("Error adding product:", error)
+  }
+}
+
+
+// Adds product to cart
 function addToCart(product) {
+  // Get data from local storage
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-  const existingProductIndex = cart.findIndex(item => JSON.stringify(item.product) === JSON.stringify(product));
-
-  if (existingProductIndex !== -1) {
-    let existingProduct = cart[existingProductIndex];
+  // Check if cart already has product selected
+  const existingProductId = cart.findIndex(item => JSON.stringify(item.product) === JSON.stringify(product));
+  // If it exists add onto stock
+  if (existingProductId !== -1) {
+    let existingProduct = cart[existingProductId];
     let updatedQuantity = existingProduct.quantity + 1;
-
+    // Checks if cart already reached the max stock of the item
     if (updatedQuantity > product.stock) {
       alert("You already have all the stock in your cart");
       existingProduct.quantity = product.stock;
     } else {
       existingProduct.quantity = updatedQuantity;
     }
-    cart[existingProductIndex] = existingProduct;
+    cart[existingProductId] = existingProduct;
   } else {
+    // If its the first one, push product into cart
     cart.push({
       product: product,
       quantity: 1
     });
   }
+  // Store back into localStorage
+  localStorage.setItem('cart', JSON.stringify(cart));
+  cartBalanceUpdate();
+}
+
+// Function that updates updated items and removes items that have been removed
+async function updateCartItems() {
+  // Get data from api and local storage
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  const products = await fetchProducts();
+
+  // Filter out items that are removed
+  cart = cart.filter(item => {
+    let exists = products.some(product => item.product._id === product._id);
+    // If it exists in api, return item into cart. Else remove it
+    return exists;
+  });
+
+  // Updates items that have been changed
+  cart.forEach(item => {
+    products.forEach(product => {
+      // If product and item id match, update the one in cart
+      if (item.product._id === product._id) {
+        item.product = product;
+        // If item quantity in cart is higher than stock, lower them down to current stock
+        if (item.quantity > item.product.stock) {
+          item.quantity = item.product.stock;
+        }
+      }
+    });
+  });
+  // Save to local storage again and update cart visual
   localStorage.setItem('cart', JSON.stringify(cart));
   cartBalanceUpdate();
 }
 
 
+// Updates balance of cart to match what's in the cart
 function cartBalanceUpdate() {
+  // Gets data from local storage
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  // Loop that adds the total sum into cartbalance
   let cartBalance = 0;
   cart.forEach((item) => {
     cartBalance += item.product.price * item.quantity;
   })
-  document.getElementById("cartBalance").innerHTML = cartBalance.toFixed(2) + " kr";
+  // Put it back into cartBalance in html with 2 decimals
+  document.getElementById("cartBalance").innerHTML = "$" + cartBalance.toFixed(2);
 }
