8806aaf5c4df18a54bfea917ef1c92593c047245
Herman0404 <herminator04@gmail.com>
2025-04-10 14:15:51 +0200

    .

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -35,9 +35,9 @@ function loadOrderItems() {
                 itemDisplay.innerHTML = `
                 <div class="order-item">
                 <img src="${img}" alt="Bild på ${name}" class="checkout-img">
-                <h2>${name}</h2>
                 <h3>${quantity}st</h3>
-                <h3>$${price * quantity}</h3>
+                <h3>${name}</h3>
+                <h3>$${(price * quantity).toFixed(2)}</h3>
                 </div>
                 `
                 orderContainer.appendChild(itemDisplay);
@@ -54,26 +54,32 @@ function loadOrderItems() {
 }
 
 document.getElementById("purchase-button").addEventListener("click", async function () {
-    let cart = localStorage.getItem("cart");
-    if (cart.length > 0) {
-        let cartItems = JSON.parse(cart) || [];
-        let formattedCart = {
-            items: cartItems.map(item => ({
-                product: item.product._id,
-                quantity: item.quantity
-            }))
-        };
-        const orderSuccess = await buyItems(formattedCart);
-        if (orderSuccess) {
-            alert("Congrats on your order!")
-            localStorage.setItem("cart", "[]");
-            cartBalanceUpdate();
-        } else {
-            alert("there has been an error with your order, please try again later.")
-        }
-    }
-    else {
-        alert("your cart is empty")
-    }
+    const confirmed = window.confirm("Vill du slutföra köpet?");
+    if (confirmed) {
+        let cart = localStorage.getItem("cart");
+        if (cart && cart.length > 0) {
+            let cartItems = JSON.parse(cart) || [];
+            let formattedCart = {
+                items: cartItems.map(item => ({
+                    product: item.product._id,
+                    quantity: item.quantity
+                }))
+            };
 
-})      
+            const orderSuccess = await buyItems(formattedCart);
+
+            if (orderSuccess) {
+                alert("Köp slutfört! Tack för att du handlade hos Hakims Liv.");
+                localStorage.setItem("cart", "[]");
+                cartBalanceUpdate();
+                window.location.href = "index.html";
+            } else {
+                alert("There has been an error with your order, please try again later.");
+            }
+        } else {
+            alert("Your cart is empty.");
+        }
+    } else {
+        console.log("Purchase was canceled.");
+    }
+});
